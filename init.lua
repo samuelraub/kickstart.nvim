35557dffd1f2fa2a6cb3608657fac350ca644774
Samuel Raub <samuel.raub@gmail.com>
2025-07-26 08:03:15 +0200

    custom highlighting for telekasten files

--- init.lua
+++ init.lua
@@ -1050,5 +1050,17 @@ require('lazy').setup({
   },
 })
 
+-- TODO: put in custom init
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.telekasten = {
+  install_info = {
+    url = '~/dev/tree-sitter-telekasten', -- local path or git repo
+    files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    branch = 'main', -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ queries/telekasten/highlights.scm
@@ -0,0 +1,8 @@
+(todo_definition) @text
+(done_definition) @comment
+(clock_definition) @text.note
+
+(h1_definition) @text.title.1.markdown
+(h2_definition) @text.title.2.markdown
+(h3_definition) @text.title.3.markdown
+
