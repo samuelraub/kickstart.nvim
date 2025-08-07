f56dfd668fc74a6ed64c4e19d07920afeb2fddf0
Samuel Raub <samuel.raub@gmail.com>
2025-08-07 08:55:14 +0200

    switch to `standardrb` as LSP and formatter

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -1,6 +1,10 @@
 vim.lsp.enable 'ruby_lsp'
 vim.lsp.config('ruby_lsp', {
   cmd = { vim.fn.expand '~/.rbenv/shims/ruby-lsp' },
+  init_options = {
+    formatter = 'standard',
+    linters = { 'standard' },
+  },
 })
 
 vim.lsp.enable 'vtsls'
@@ -34,9 +38,11 @@ vim.lsp.enable 'biome'
 
 vim.lsp.enable 'marksman'
 
+-- needed for some commands like `organize_imports`
 return {
   'yioneko/nvim-vtsls',
 }
 
 -- pnpm add -g vscode-langservers-extracted yaml-language-server @vtsls/language-server @microsoft/compose-language-service dockerfile-language-server-nodejs @tailwindcss/language-server @biomejs/biome
 -- brew install marksman prettierd
+-- gem install ruby-lsp
