368c8cfcb4f55600ae6c3b73d46ace44f9885eb6
Samuel Raub <samuel.raub@gmail.com>
2025-08-19 08:50:01 +0200

    add fuzzy refine to `live_grep_args`

--- init.lua
+++ init.lua
@@ -364,34 +364,50 @@ require('lazy').setup({
       -- Telescope picker. This is really useful to discover what Telescope can
       -- do as well as how to actually do it!
 
+      -- Open multiple files if selected with TAB
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          require('telescope.actions').close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format('%s %s', 'edit', j.path))
+            end
+          end
+        else
+          require('telescope.actions').select_default(prompt_bufnr)
+        end
+      end
+
+      local lga_actions = require 'telescope-live-grep-args.actions'
+
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {
-        -- find_files = {
-        --   find_command = {
-        --     'rg',
-        --     '--files',
-        --     '--hidden',
-        --     '--no-ignore-vcs',
-        --     '-g',
-        --     '!**/node_modules/*',
-        --     '-g',
-        --     '!**/.git/*',
-        --   },
-        -- },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<CR>'] = select_one_or_multi,
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
+          },
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ['<C-k>'] = lga_actions.quote_prompt(),
+                ['<C-i>'] = lga_actions.quote_prompt { postfix = ' --iglob ' },
+                ['<C-Space>'] = require('telescope.actions').to_fuzzy_refine,
+              },
+            },
           },
         },
       }
